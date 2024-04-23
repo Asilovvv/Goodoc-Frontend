783182aaf2c01f3a6daa469e3ef1533a8810e5d4
Asilovvv <bekturasilov@gmail.com>
2024-04-23 16:53:24 +0600

    created goodoc path

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Button from '@mui/material/Button';
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <div>hello world</div>
+     <Button variant="contained">Contained</Button>
+    </>
+  )
+}
+
+export default App
