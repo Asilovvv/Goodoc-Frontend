e90bd728f437018e450be2a84d3ca656791aa483
Asilovvv <bekturasilov@gmail.com>
2024-04-24 14:54:02 +0600

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import { useState } from 'react'
 import Button from '@mui/material/Button';
+import Logo from './images/logo.png';
+import Time from './images/time.png';
+import Med from './images/image1.png';
 
 import './App.css'
 
 function App() {
   return (
     <>
-     <div>hello world</div>
+     {/* <div>hello world</div> */}
+     <img src={Time} alt="Time"/>
+     <img src={Logo} alt="Logo"/>
+     <img src={Med} alt="Med"/>
      <Button variant="contained">Contained</Button>
+     <Button variant="contained">Contain</Button>
+     <Button variant="contained">Containssss</Button>
+     <Button variant="contained">Salam</Button>
+     <Button variant="contained">Aleikum</Button>
     </>
   )
-}
 
+}
 export default App
